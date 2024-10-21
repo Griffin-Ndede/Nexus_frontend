e8273aaa90eda2d1fba3a4c12f934b20a28c1721
Griffin <omondigriffin4@gmail.com>
2024-10-21 17:25:15 +0200

    creates browser routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '/src/index.css'
+import { BrowserRouter, Route , Routes} from 'react-router-dom'
 import Home from './Components/Home'
 import Dashboard from './Components/Dashboard'
 
@@ -7,8 +8,12 @@ function App() {
 
   return (
     <>
-    <Home/>
-    <Dashboard/>
+    <BrowserRouter>
+    <Routes>
+      <Route path = "" element = {<Home/>}></Route>
+      <Route path = "/dashboard" element = {<Dashboard/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
